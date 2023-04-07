7175c498a7f0bd9e67777f394f5872713e140e85
gaboliveiradev <g38789138@gmail.com>
2023-04-07 09:24:41 -0300

    aplicando cor na nossa homepage + tirando o banner de debug do app

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor:
+          Colors.purple[800], // [800] Deixar a nossa cor um pouco mais escura.
+    );
   }
 }
