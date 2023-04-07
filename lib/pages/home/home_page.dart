a2d8c3c981673bb5f6f1c199ab472e9da675aac7
gaboliveiradev <g38789138@gmail.com>
2023-04-07 12:30:34 -0300

    trocando nossa homePage para StateFulWidget

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,15 +1,27 @@
-import 'package:app_nubank/main.dart';
 import 'package:app_nubank/pages/home/widgets/my_app_bar.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late bool _showMenu;
+
+  @override
+  void initState() {
+    super.initState();
+    _showMenu = false;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          Colors.purple[800], // [800] Deixar a nossa cor um pouco mais escura.
+      // [800] Deixar a nossa cor um pouco mais escura.
+      backgroundColor: Colors.purple[800],
       body: Stack(
         alignment: Alignment.topCenter,
         children: const <Widget>[
