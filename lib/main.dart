84ea7b3d6d3fb7b7a583338b79dd11aefd16c235
gaboliveiradev <g38789138@gmail.com>
2023-04-07 10:40:35 -0300

    alterando a cor dos icones de bateria, wifi....

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:app_nubank/pages/home/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  // statusBarBrightness => Altera a cor dos icones de bateria, wifi...
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarBrightness: Brightness.dark));
   runApp(const MyApp());
 }
 
@@ -15,6 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Nubank - Design',
       theme: ThemeData(
         primarySwatch: Colors.purple,
+        brightness: Brightness.dark,
       ),
       home: HomePage(),
     );
